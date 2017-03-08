244bbe31472249c229f96d6f0ef4b957cbf751c5
Jason <jisler3@gatech.edu>
2017-03-08 14:49:25 -0500

    Added CollectionView methods.

--- Meme 2.0/SentMemesCollectionViewController.swift
+++ Meme 2.0/SentMemesCollectionViewController.swift
@@ -21,6 +21,31 @@ class SentMemesCollectionViewController : UICollectionViewController {
         memes = applicationDelegate.memes
     }
     
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.memes.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemesCollectionViewCell", for: indexPath) as! MemesCollectionViewCell
+//        let villain = self.memes[(indexPath as NSIndexPath).row]
+//        
+//        // Set the name and image
+//        cell.nameLabel.text = villain.name
+//        cell.villainImageView?.image = UIImage(named: villain.imageName)
+//        //cell.schemeLabel.text = "Scheme: \(villain.evilScheme)"
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath:IndexPath) {
+        
+//        let detailController = self.storyboard!.instantiateViewController(withIdentifier: "VillainDetailViewController") as! VillainDetailViewController
+//        detailController.villain = self.allVillains[(indexPath as NSIndexPath).row]
+//        self.navigationController!.pushViewController(detailController, animated: true)
+        
+    }
+
     
     
     
